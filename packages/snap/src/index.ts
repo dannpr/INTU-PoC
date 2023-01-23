b52540ef86c90f81a39b1cdc58f9639642597592
danny ba <dannba0@gmail.com>
2023-01-23 11:31:34 +0100

    new things

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -54,7 +54,7 @@ export const getEOAInfo = async () => {
 
 export const signing = async (EOA: string, message: string) => {
   const response: any = await wallet.request({
-    method: 'snap_signMessage',
+    method: 'snap_si—gnMessage',
     params: [EOA, message],
   });
   console.log('signing response', response);
